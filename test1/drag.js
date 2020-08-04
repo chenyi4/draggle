0219c5a099ee4b0c8dfac6cae36d6a6efdc2292d
chenyi <chenyi4@qq.com>
2020-08-04 10:17:36 +0800

    新增修改

---
+++ test1/drag.js
@@ -0,0 +1,23 @@
+/**
+ * A simple, efficent mobile slider solution
+ * @file drag.js
+ * @author CHENYI
+ *    chenyi chenyi4@qq.com
+ *
+ * @LICENSE https://github.com/BE-FE/iSlider/blob/master/LICENSE
+ */
+
+ 
+(function (global) {
+    'use strict';
+
+    var drag = function () {
+        this.name = "sdss";
+    }
+
+
+    if(!window.drag){
+        window.drag = drag;
+    }
+
+})(window || this);
